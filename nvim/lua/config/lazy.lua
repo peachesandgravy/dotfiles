72df26687ec45b638ef303985f488cf893a6e868
Livi <olivia.pelley@icloud.com>
2026-07-29 12:08:38 -0400

    added transparent background and highlighting

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -35,6 +35,7 @@ vim.o.termguicolors = true
 vim.o.showmode = false
 vim.o.wrap = true
 vim.o.clipboard = "unnamedplus"
+vim.o.cursorline = true
 
 -- Setup lazy.nvim
 require("lazy").setup({
@@ -49,16 +50,86 @@ require("lazy").setup({
   checker = { enabled = true },
 })
 
--- Set colorscheme
-vim.cmd('colorscheme catppuccin-mocha')
+require("catppuccin").setup({
+  flavour = "auto", -- latte, frappe, macchiato, mocha
+  background = { -- :h background
+    light = "latte",
+    dark = "mocha",
+  },
+  transparent_background = true, -- disables setting the background color.
+  float = {
+    transparent = false, -- enable transparent floating windows
+    solid = false, -- use solid styling for floating windows, see |winborder|
+  },
+  term_colors = false, -- sets terminal colors (e.g. `g:terminal_color_0`)
+  dim_inactive = {
+    enabled = false, -- dims the background color of inactive window
+    shade = "dark",
+    percentage = 0.15, -- percentage of the shade to apply to the inactive window
+  },
+  no_italic = false, -- Force no italic
+  no_bold = false, -- Force no bold
+  no_underline = false, -- Force no underline
+  styles = { -- Handles the styles of general hi groups (see `:h highlight-args`):
+    comments = { "italic" }, -- Change the style of comments
+    conditionals = { "italic" },
+    loops = {},
+    functions = {},
+    keywords = {},
+    strings = {},
+    variables = {},
+    numbers = {},
+    booleans = {},
+    properties = {},
+    types = {},
+    operators = {},
+    -- miscs = {}, -- Uncomment to turn off hard-coded styles
+  },
+  lsp_styles = { -- Handles the style of specific lsp hl groups (see `:h lsp-highlight`).
+    virtual_text = {
+      errors = { "italic" },
+      hints = { "italic" },
+      warnings = { "italic" },
+      information = { "italic" },
+      ok = { "italic" },
+    },
+    underlines = {
+      errors = { "underline" },
+      hints = { "underline" },
+      warnings = { "underline" },
+      information = { "underline" },
+      ok = { "underline" },
+    },
+    inlay_hints = {
+      background = true,
+    },
+  },
+  color_overrides = {},
+  custom_highlights = {},
+  auto_integrations = true,
+  integrations = {
+    cmp = true,
+    gitsigns = true,
+    nvimtree = true,
+    notify = false,
+    mini = {
+      enabled = true,
+      indentscope_color = "",
+    },
+    -- For more plugins integrations please scroll down (https://github.com/catppuccin/nvim#integrations)
+  },
+})
+
+-- setup must be called before loading
+vim.cmd.colorscheme "catppuccin-nvim"
 
 -- Setup lualine
 require('lualine').setup {
   options = {
     icons_enabled = false,
     theme = 'auto',
---    component_separators = { left = '', right = ''},
---    section_separators = { left = '', right = ''},
+    --    component_separators = { left = '', right = ''},
+    --    section_separators = { left = '', right = ''},
     component_separators = { left = '|', right = '|'},
     section_separators = { left = '', right = ''},
     disabled_filetypes = {
@@ -93,7 +164,7 @@ require('lualine').setup {
     lualine_b = {'branch', 'diff', 'diagnostics'},
     lualine_c = {'filename'},
     lualine_x = {'encoding', 'fileformat', 'filetype'},
---    lualine_x = {'filetype'},
+    --    lualine_x = {'filetype'},
     lualine_y = {'progress'},
     lualine_z = {'location'}
   },
--- nvim/lua/plugins/spec.lua
+++ nvim/lua/plugins/spec.lua
@@ -8,6 +8,4 @@ return {
 
   { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
 
-  { "typicode/bg.nvim", lazy = false },
-
 }
