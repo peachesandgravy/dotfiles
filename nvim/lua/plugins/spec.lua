39d1e42f52ba31c2b6a0b1ba41472eb27e417cf6
Livi <olivia.pelley@icloud.com>
2026-08-04 03:10:33 -0400

    init

---
+++ nvim/lua/plugins/spec.lua
@@ -0,0 +1,9 @@
+return {
+
+  { 'nvim-lualine/lualine.nvim', dependencies = { 'nvim-tree/nvim-web-devicons' } },
+
+  { "RRethy/base16-nvim" },
+
+  { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
+
+}
