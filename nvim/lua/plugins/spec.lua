4e75fce10cf3ee016908aa48bc9358e65f7aeac8
Livi <olivia.pelley@icloud.com>
2026-08-04 03:21:21 -0400

    deleted

--- nvim/lua/plugins/spec.lua
+++
@@ -1,9 +0,0 @@
-return {
-
-  { 'nvim-lualine/lualine.nvim', dependencies = { 'nvim-tree/nvim-web-devicons' } },
-
-  { "RRethy/base16-nvim" },
-
-  { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
-
-}
